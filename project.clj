81a7cc2c9deb060b137eeb6ad282ccbbd8fb2b72
Logan Senjov <logansenjov@gmail.com>
2016-11-12 00:36:48 +1000

    Damn I hate the default style. More lines is fine by me

--- project.clj
+++ project.clj
@@ -47,7 +47,8 @@
                            ;; in the default browser once Figwheel has
                            ;; started and complied your application.
                            ;; Comment this out once it no longer serves you.
-                           ;; :open-urls ["http://localhost:3449/index.html"]}
+                           ;; :open-urls ["http://localhost:3449/index.html"]
+                           }
 
                 :compiler {:main acmoi.frontend.core
                            :asset-path "js/compiled/out"
